593502eb7dde0fb0f63a364fb0a52705938b8f15
malte_kleine <kleine@sonah.tech>
2025-09-22 13:59:40 +0200

    colorspace

--- cv_and_pil.py
+++ cv_and_pil.py
@@ -9,20 +9,23 @@ img_lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
 cv2.imwrite(image_path, image)
 
 
+coordsPts = np.array(self.__highlightConfig[imgSource][roiId]["coords"])
 
-coords_pts = np.array([[0.4, 0.4], [0.6, 0.4], [0.6, 0.6], [0.4, 0.6]])
+pts = (coordsPts * np.array([image.shape[1], image.shape[0]])).astype(np.int32)
+self.__roiMasks[imgSource] = cv2.fillConvexPoly(roiMask, pts, color=(1, 1, 1))
+self.__highlightMasks[imgSource] = np.multiply(self.__roiMasks[imgSource], np.array(self.__highlightColor, dtype=np.uint8))
 
-pts = (coords_pts * np.array([image.shape[1], image.shape[0]])).astype(np.int32)
-roiMasks[imgSource] = cv2.fillConvexPoly(roiMask, pts, color=(1, 1, 1))
-highlightMasks[imgSource] = np.multiply(self.__roiMasks[imgSource], np.array(self.__highlightColor, dtype=np.uint8))
-
-video = cv2.VideoCapture(videoFile)
+video = cv2.VideoCapture(videofile)
 frameId = 0
 while video.isOpened():
-    success, _ = video.read()
+    success, frame = video.read()
     if success:
         frameId += 1
     else:
         video.release()
         break
-return frameId
+
+colorspace = "rgb"
+image = cv2.cvtColor(image, getattr(cv2, "COLOR_BGR2" + colorspace.upper()))
+_, encodedImage = cv2.imencode(".jpg", image)
+imageBytes = encodedImage.tobytes()
